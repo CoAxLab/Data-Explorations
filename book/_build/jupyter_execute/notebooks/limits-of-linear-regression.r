d27decdcc57830158d4d22ab53b527269119a13c
timothyv <timothyv@gmail.com>
2022-02-17 04:39:02 -0500

    Feb update

--- book/_build/jupyter_execute/notebooks/limits-of-linear-regression.r
+++ book/_build/jupyter_execute/notebooks/limits-of-linear-regression.r
@@ -1,86 +1,29 @@
-# Load the namespace for these packages and attach them to the search list 
+# Libaries to install
+#install.packages("glmnet")
+#install.packages("matrixStats")
+#install.packages("denoiseR")
+#install.packages("gplots")
+#install.packages("RColorBrewer")
+#install.packages("plot3D") 
+# install.packages("car") 
+#install.packages("ISLR")
+
+# Load the libraries
 library(MASS)
+library(glmnet)
+library(tidyverse)
 library(ggplot2)
+library(matrixStats)
+library(denoiseR)
+library(gplots)
+library(RColorBrewer)
+library(plot3D) 
+library(ISLR)
 
-# fix(Cars93)
 
-names(Cars93)
+# Set the random number generator seed
+set.seed(2022)
 
-help(Cars93)
-?Cars93
-
-# To get more information on the lm function execute this cell
-help(lm)
-
-options(repr.plot.width=5, repr.plot.height=5) # plot size 
-ggplot(Cars93, aes(sample=Weight)) + geom_point(stat = "qq") + ggtitle("Normal dist. vs. normal sample")
-
-# install.packages("repr") # uncomment to install repr library
-library(repr)
-options(repr.plot.width=10, repr.plot.height=10) # plot size reset
-pairs(Cars93)
-
-# Plot the joint distribution
-options(repr.plot.width=8, repr.plot.height=8)
-ggplot(aes(Width, Weight), data=Cars93) + 
-geom_point() + 
-xlab('Width') + 
-ylab('Weight')
-
-# Let's try a simple linear model
-lm.fit = lm(Weight~Length, data=Cars93)
-
-# In order to see the results, use the summary function
-summary(lm.fit)
-
-# you can also add the fit model in your ggplot to show both the line fitted and uncertainty around the fitted line
-ggplot(aes(Length, Weight), data=Cars93) + 
-  geom_point() + 
-  geom_smooth(method = 'lm',se=FALSE) # fits line without standard error shading - try deleting "se=FALSE"
-
-# Just grab the regression coefficients themselves
-coef(lm.fit)[1]
-
-coef(lm.fit)[2]
-
-# In order to estimate the confidence interval on the regression coefficients we will use confit
-# help("confint") # uncomment to see information on this function
-print(confint(lm.fit))
-
-# We want to predict car weight at specific lstat levels
-# help("predict") # Uncomment to see how the function works
-
-# Here we will predict what the weight would be if the width for the car is 165, 180, or 195 inches.
-
-length_input_values = data.frame(Length=c(165,180,195)) #specific SES values that you want a prediction for 
-prediction_table = data.frame(predict(lm.fit, length_input_values, interval = 'confidence'))  #calling the predict function 
-
-pred_input_table = cbind(length_input_values, prediction_table) #bind the input and prediction dataframes by column 
-
-colnames(pred_input_table) = c('length_input','predicted fit','lower_CI_bound', 'upper_CI_bound') #use informative column names 
-round(pred_input_table,2) #display rounded values
-
-# help("abline") # Uncomment to see how the function works
-# abline(lm.fit)
-
-ggplot(aes(`Length`, `Weight`), data=Cars93) + 
-  geom_point() + 
-  geom_abline(intercept = coef(lm.fit)[1], slope = coef(lm.fit)[2]) + # adds a line that shows the model's fit
-  xlab('Car Length') + ylab('Car Weight') 
-
-install.packages("ggfortify")
-library(ggfortify) #load a ggplot library that can plot model diagnostics  
-
-suppressWarnings(
-  autoplot(lm.fit, label.size = 3)
-  )
-# ( ^ note: I just suppressed a warning about an old version of a command that ggfortify uses - you don't need to worry about it)
-
-# Let's plot some relationships first.
-install.packages("plot3D") # Uncomment if this is not installed
-
-library(plot3D) # note - this is outside of the ggplot2 package
-# Let's look at the relationship between Length AND Width on car weight
 
 scatter3D(Cars93$Width, Cars93$Length, Cars93$Weight, phi=20, theta=20, xlab="Width", ylab="Length",zlab="Weight")
 #phi controls tilt and theta controls angle
@@ -103,11 +46,7 @@ summary(lm.fit_new)
 # First we will want to clear the workspace
 #rm(list=ls())
 
-# Next load the car package for this
-install.packages("car") #Uncomment if you haven't already installed the package
 
-install.packages("ISLR")
-library(ISLR)
 
 # Look at the Carseats dataset
 # help(Carseats) # Uncomment to view documentation
@@ -122,3 +61,139 @@ contrasts(Carseats$ShelveLoc)
 #here, bad is when good and medium are 0 
 #good is when good = 1 and medium = 0 
 #medium is when good = 0 and medium = 1
+
+make_data <- function(n,p,k,s){
+  
+  if (n < p){
+    # If number of features is greater 
+    # than number of observations, increase
+    # observations for the PCA to work
+    m=p
+  } else {
+    # Otheriwse use the input n
+    m=n
+  }
+  
+  if (p < k) {
+    # Make full rank until p=k
+    # degree_diff = k-p
+    x = LRsim(m,p,p,s)$X
+  } else {
+    x = LRsim(m,p,k,s)$X
+  }
+
+  # Recover low-d components
+  z = princomp(x)
+  
+  # Set the first k components to 1, otherwise 0
+  if (p-k < 0){
+    u = rep(10,p) #rnorm(p,mean=1,sd=0.5)
+  } else {
+    u = c(rep(10,k),matrix(0,nrow=1,ncol=p-k))
+  }
+  
+  # Calculate the weights in the data space
+  b = t(u %*% t(z$loadings))
+  
+  # In case we needed to set m=p for the 
+  # PCA to work, take the first n observations
+  # of x
+  x = x[1:n,]
+  
+  # Generate your output
+  y = x %*% b + rnorm(n)
+  
+  return(list(X=x, Y=y, B=b))
+}
+
+n = 500
+p = 20
+k = 10
+s = 20
+data = make_data(n,p,k,s)
+summary(lm(Y~X, data=data))
+
+heatmap.2(cor(data$X),col=brewer.pal(11,"RdBu"), trace="none", 
+          key.title = NA, key.ylab = NA, key.xlab = "Correlation")
+
+k = p 
+data = make_data(n,p,k,s)
+heatmap.2(cor(data$X),col=brewer.pal(11,"RdBu"), trace="none", 
+          key.title = NA, key.ylab = NA, key.xlab = "Correlation")
+
+## ------------------------------
+# LM split validation function
+## ------------------------------
+bv_lm <- function(n, degree, k, s) {
+    # Set up the arrays for storing the results
+    train_rss = matrix(data=NA,nrow=degree,ncol=1)
+    test_rss = matrix(data=NA,nrow=degree,ncol=1)
+    p_max = degree[length(degree)]
+
+    # Loop through for each set of p-features
+    for (p in degree) {
+        # Set up the data, split into training and test
+        # sets of size n
+        data = make_data(n*2,p_max,k,s)
+
+        train = list(X=data$X[1:n,1:p],Y = data$Y[1:n])
+        test = list(X=data$X[(n+1):nrow(data$X),1:p],Y = data$Y[(n+1):nrow(data$X)])
+
+        # Use simple GLM
+        lm.fit = lm(Y ~ X, data=train) #, subset=set_id)
+    
+        # Get your model prediction on both the training
+        # and test sets
+        yhat_train = predict(lm.fit)
+        yhat_test  = predict(lm.fit, newdata=test)
+
+        # Because we get weird outlier predictions plot median sum square error
+        train_rss[p-degree[1]+1] = median((train$Y - yhat_train)^2)
+        test_rss[p-degree[1]+1] = median((test$Y - yhat_test)^2)
+    }
+  
+    # Store the RSS in a data frame
+    out = data.frame(p=degree, train=train_rss, test=test_rss)
+}
+
+n = 500
+k = 10 
+s = 20
+degree = seq(2,20)
+
+bv_df = bv_lm(n,degree,k, s)
+
+ggplot(bv_df, aes(x=p)) +
+  geom_line(aes(y = test), color = "darkred") +
+  ylab("Median sum squared error") + xlab("p")
+
+options(warn=-1)
+
+# Parameters
+n_iter = 2000
+n = 500
+k = 10 
+s = 20
+degree = seq(2,20)
+
+# Aggregated output
+train_rss = matrix(data=NA,nrow=length(degree),ncol=n_iter)
+test_rss = matrix(data=NA,nrow=length(degree),ncol=n_iter)
+
+# Loop through n_iter times
+for (i in 1:n_iter) {
+  bv_df = bv_lm(n,degree,k,s)
+  train_rss[,i]=bv_df$train
+  test_rss[,i] =bv_df$test
+}
+
+# Make a new data frame for plotting
+run_df = data.frame(p=degree, train=rowMeans(train_rss), test=rowMeans(test_rss),
+                    strain=rowSds(train_rss), stest=rowSds(test_rss))
+
+# Plot
+ggplot(run_df, aes(x=p)) +
+  geom_line(aes(y = test), color = "darkred") +
+  geom_line(aes(y = train), color="steelblue", linetype="twodash") +
+   ylab("Median sum squared error") + xlab("p") 
+
