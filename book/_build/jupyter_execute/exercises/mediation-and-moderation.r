39add7b89f77bf5f2f148ceb5def93d0a50a362d
Tim Verstynen <timothyv@gmail.com>
2023-02-06 09:54:13 -0500

    mediation exercise push

---
+++ book/_build/jupyter_execute/exercises/mediation-and-moderation.r
@@ -0,0 +1,39 @@
+sample_size = 100 # How many children in data set? 
+age_lo = 80     # minimum age, in months
+age_hi = 200    # maximum age, in months
+beta_xa = 0.5   # amount by which experience changes for increase of one month in age
+beta_x0 = -5    # amount of experience when age = 0 (not interpretable, since minimum age for this data is 80 months)
+sd_x = 50       # standard dev of gaussian noise term, epsilon_x
+beta_ca = 0.8   # amount that comprehension score improves for every increase of one unit in age
+beta_cx = 3     # amount that comprehension score improves for every increase of one unit in reading experience
+beta_c0 = 10    # comprehension score when reading experience is 0. 
+sd_c = 85      # standard dev of gaussian noise term, epsilon_c
+
+simulate_data <- function(sample_size, age_lo, age_hi, beta_xa, 
+                          beta_x0, sd_x, beta_ca, beta_cx, beta_c0, sd_c) {
+      # WRITE YOUR CODE HERE
+      
+}
+
+#dat <- simulate_data(sample_size, age_lo, age_hi, beta_xa, beta_x0, sd_x, beta_ca, beta_cx, beta_c0, sd_c)
+#head(dat)
+
+# WRITE YOUR CODE HERE
+
+
+# WRITE YOUR CODE HERE
+
+
+# WRITE YOUR CODE HERE
+
+
+
+# WRITE YOUR CODE HERE
+
+
+
+# WRITE YOUR CODE HERE
+
+
+> *Write your response here* 
+> 
--- book/discussions/techniques-for-data-cleansing.md
+++ book/discussions/techniques-for-data-cleansing.md
@@ -1,5 +1,5 @@
 # Discussion questions
 
-1. Provide an example for how each type of anomaly in your data tables (semantic, syntactic, coverage) can impact the interpretation of the relationship between a response variable (Y) and a set of predictor variables (X). 
+1. Provide a hypothetical example of each type of anomaly in your data tables (semantic, syntactic, coverage) and show how it can impact the interpretation of the relationship between a response variable (Y) and a set of predictor variables (X). 
 
-2. How can data cleansing pipelines potentially harm the mapping between the “real world” and your data table? Provide a specific example to illustrate your answer.
+2. How can errors in your data cleansing pipeline potentially harm the mapping between the “real world” and your data table? Provide a specific example to illustrate your answer.
