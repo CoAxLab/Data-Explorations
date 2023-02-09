b4438a12032cf61c054d03789dada5bf5783cf9b
Tim Verstynen <timothyv@gmail.com>
2023-02-09 06:23:00 -0500

    gdown fix

--- book/_build/jupyter_execute/exercises/linear-models.r
+++ book/_build/jupyter_execute/exercises/linear-models.r
@@ -1,6 +1,12 @@
-#INSERT CODE HERE
+# If you are running this on your local computer, wet your workign directory to 
+# the location of the lexDat data by setting your harddrive. Uncomment this line
+# and change the location to where it is on your computer. 
+#setwd("~/Documents/PittCMU/G3/DSPN/DataSciencePsychNeuro/Homeworks/hcp_data")
 
+# If you are running this on Colab, then use something like this.
+# system("gdown --id 1hywRmGdvhbDYTrQRyl1_bLJsq-T3GJq2")
 
+# INSERT CODE HERE
 
 #INSERT CODE HERE
 
